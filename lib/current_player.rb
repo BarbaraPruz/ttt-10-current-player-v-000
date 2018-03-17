4d74a8ccfe2702f9dca6c40e17e1d8b9db584082
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-17 23:22:53 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,9 @@
 def turn_count (board)
   count = 0 
-  board.each { |pos| 
+  board.each { |pos| if pos==' ' || pos== '' count+=1 end}
+  return count
+end
+
+def current_player (board)
+  return turn_count(board)%2 == 0 ? "X" : "O"
+end
